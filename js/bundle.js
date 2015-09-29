b473128d5ca657ea3e29315fb9745607716ccbde
Avizura <michael.dovzhenko@gmail.com>
2015-09-29 13:06:46 +0300

    opt

--- js/bundle.js
+++ js/bundle.js
@@ -459,31 +459,26 @@ var Cell = React.createClass({displayName: "Cell",
 
     getInitialState: function () {
         return {
-            isEditMode: false,
             data: ""
         };
     },
     componentWillMount: function () {
         this.setState({
-            isEditMode: this.props.isEditMode,
             data: this.props.data
         });
     },
     handleEditCell: function (evt) {
-        this.setState({isEditMode: true});
     },
     handleKeyDown: function (evt) {
         switch (evt.keyCode) {
             case 13: // Enter
             case 9: // Tab
                 evt.target.blur();
-                this.setState({isEditMode: false});
                 break;
         }
     },
     save : function() {
       Actions.updateCell(this.props.id, this.props.column, this.state.data);
-      this.setState({isEditMode: false});
     },
 
     handleChange: function (evt) {
@@ -491,16 +486,6 @@ var Cell = React.createClass({displayName: "Cell",
     },
 
     render: function () {
-        // var cellHtml;
-        // if (this.state.isEditMode) {
-        //     cellHtml = <input type='text' value={this.state.data}
-        //         onKeyDown={this.handleKeyDown} onBlur={this.save} onChange={this.handleChange} autoFocus={true} />
-        // }
-        // else {
-        //     cellHtml = <div onDoubleClick={this.handleEditCell}>{this.state.data}</div>
-        // }
-        // return (<td>{cellHtml}</td>);
-        // return(<td contentEditable="true" onChange={this.handleChange} onKeyDown={this.handleKeyDown}>{this.state.data}</td>);
         return(React.createElement("td", null, React.createElement("input", {type: "text", value: this.state.data, onChange: this.handleChange, onKeyDown: this.handleKeyDown, onBlur: this.save})));
     }
 });
--- js/components/Cell.react.js
+++ js/components/Cell.react.js
@@ -5,31 +5,26 @@ var Cell = React.createClass({
 
     getInitialState: function () {
         return {
-            isEditMode: false,
             data: ""
         };
     },
     componentWillMount: function () {
         this.setState({
-            isEditMode: this.props.isEditMode,
             data: this.props.data
         });
     },
     handleEditCell: function (evt) {
-        this.setState({isEditMode: true});
     },
     handleKeyDown: function (evt) {
         switch (evt.keyCode) {
             case 13: // Enter
             case 9: // Tab
                 evt.target.blur();
-                this.setState({isEditMode: false});
                 break;
         }
     },
     save : function() {
       Actions.updateCell(this.props.id, this.props.column, this.state.data);
-      this.setState({isEditMode: false});
     },
 
     handleChange: function (evt) {
@@ -37,16 +32,6 @@ var Cell = React.createClass({
     },
 
     render: function () {
-        // var cellHtml;
-        // if (this.state.isEditMode) {
-        //     cellHtml = <input type='text' value={this.state.data}
-        //         onKeyDown={this.handleKeyDown} onBlur={this.save} onChange={this.handleChange} autoFocus={true} />
-        // }
-        // else {
-        //     cellHtml = <div onDoubleClick={this.handleEditCell}>{this.state.data}</div>
-        // }
-        // return (<td>{cellHtml}</td>);
-        // return(<td contentEditable="true" onChange={this.handleChange} onKeyDown={this.handleKeyDown}>{this.state.data}</td>);
         return(<td><input type='text' value={this.state.data} onChange={this.handleChange} onKeyDown={this.handleKeyDown} onBlur={this.save} /></td>);
     }
 });
